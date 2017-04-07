1a8b6f5461fd41b95f1f2d6b05d64f3ff8855af8
Renzo Davoli <renzo@cs.unibo.it>
2017-04-07 10:59:48 +0200

    bugfis in create_deb (for versions > 1.0)

--- create_deb.sh
+++ create_deb.sh
@@ -2,6 +2,6 @@
 
 UPSTREAM_VERSION="$(dpkg-parsechangelog --show-field Version | sed 's/-.*$//')"
 cd ..
-tar zcvf libexecs_1.0.orig.tar.gz --transform "s/^s2argv-execs/libexecs_${UPSTREAM_VERSION}/" s2argv-execs
+tar zcvf libexecs_${UPSTREAM_VERSION}.orig.tar.gz --transform "s/^s2argv-execs/libexecs_${UPSTREAM_VERSION}/" s2argv-execs
 cd s2argv-execs
 debuild -uc -us -sa
