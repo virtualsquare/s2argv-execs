eebbfdbfb249ebd103314cdc4a44ca0056575ca3
Renzo Davoli <renzo@cs.unibo.it>
2019-01-05 14:42:18 +0100

    create_deb.sh exclude debian dir and newtag arg

--- create_deb.sh
+++ create_deb.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-UPSTREAM_VERSION="$(dpkg-parsechangelog --show-field Version | sed 's/-.*$//')"
-cd ..
-tar zcvf libexecs_${UPSTREAM_VERSION}.orig.tar.gz --transform "s/^s2argv-execs/libexecs_${UPSTREAM_VERSION}/" s2argv-execs
-cd s2argv-execs
+if [ $1 == 'newtag' ]
+then
+	UPSTREAM_VERSION="$(dpkg-parsechangelog --show-field Version | sed 's/-.*$//')"
+	cd ..
+	tar zcvf libexecs_${UPSTREAM_VERSION}.orig.tar.gz --transform "s/^s2argv-execs/libexecs_${UPSTREAM_VERSION}/" --exclude "s2argv-execs/debian" --exclude "s2argv-execs/.git" s2argv-execs
+	cd s2argv-execs
+fi
+
 debuild -uc -us -sa
