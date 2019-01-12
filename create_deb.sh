aa3c3d6e514045c3ee1f8baf8bd9ba50458513af
Renzo Davoli <renzo@cs.unibo.it>
2019-01-12 18:05:11 +0100

    fix createdeb script bug (dir name in orig.tar.gz)

--- create_deb.sh
+++ create_deb.sh
@@ -4,7 +4,7 @@ if [ $1 == 'newtag' ]
 then
 	UPSTREAM_VERSION="$(dpkg-parsechangelog --show-field Version | sed 's/-.*$//')"
 	cd ..
-	tar zcvf libexecs_${UPSTREAM_VERSION}.orig.tar.gz --transform "s/^s2argv-execs/libexecs_${UPSTREAM_VERSION}/" --exclude "s2argv-execs/debian" --exclude "s2argv-execs/.git" s2argv-execs
+	tar zcvf libexecs_${UPSTREAM_VERSION}.orig.tar.gz --transform "s/^s2argv-execs/libexecs-${UPSTREAM_VERSION}/" --exclude "s2argv-execs/debian" --exclude "s2argv-execs/.git" s2argv-execs
 	cd s2argv-execs
 fi
 
